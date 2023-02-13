36805df21da06d3ccfdb5b77572bc6cf751ec8b8
VictorMFL <vm859222@gmail.com>
2023-02-13 15:08:13 -0300

    Versão para celular

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuMobile = document.querySelector(".menu-mobile");
+
+function handleClick() {
+  if (menuMobile.style.display == "none") {
+    menuMobile.style.display = "block";
+  } else {
+    menuMobile.style.display = "none";
+  }
+}
