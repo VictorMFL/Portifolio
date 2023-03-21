58759afb56237870541c596bf6ec193621190641
VictorMFL <vm859222@gmail.com>
2023-03-21 13:26:39 -0300

    nova versão do meu Portifólio

--- script.js
+++
@@ -1,9 +0,0 @@
-let menuMobile = document.querySelector(".menu-mobile");
-
-function abrirMenu() {
-  menuMobile.style.display = "block";
-}
-
-function fecharMenu() {
-  menuMobile.style.display = "none";
-}
